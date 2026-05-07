8a94d00b725d2457b0b25f7b3ce8155fcc3c043a
jjmin208 <151809776+jjmin208@users.noreply.github.com>
2026-05-07 10:44:04 +0900

    260507 chrome app

---
+++ bug.ts
@@ -0,0 +1,4 @@
+/*
+There is a bug where completed `<li>` elements disappear when the page is reloaded.However, the `<li>` elements in the todoList don't disappear even after reloading the page.
+If Nico is reading this, I would appreciate it if you could leave a suggestion for revision or any comments.
+*/
--- js/todo.js
+++ js/todo.js
@@ -1,17 +1,31 @@
 const toDoForm = document.querySelector(".main__todoForm");
 const toDoInput = document.querySelector(".main__todoForm__input");
 const toDoList = document.getElementById("todo-list");
+const completedList = document.getElementById("completed-list");
 const TODOS_KEY = "todos";
 
 let toDos = [];
+let completedToDos = [];
 
 function saveToDos() {
   localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
+function saveCompletedToDos() {
+  localStorage.setItem(COMPLETED_KEY, JSON.stringify(completedToDos));
+}
 
 function deleteToDo(event) {
-  const li = event.target.parentElement;
+  const li = event.target.parentElement.parentElement;
   li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
+}
+function completeToDo(event) {
+  const li = event.target.parentElement.parentElement;
+  li.classList.add("completed");
+  completedList.appendChild(li);
+  const completeButton = event.target;
+  completeButton.remove();
   toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
   saveToDos();
 }
@@ -21,11 +35,17 @@ function paintToDo(newTodo) {
   li.id = newTodo.id;
   const span = document.createElement("span");
   span.innerText = newTodo.text;
-  const button = document.createElement("button");
-  button.innerText = "❌";
-  button.addEventListener("click", deleteToDo);
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "❌";
+  deleteButton.addEventListener("click", deleteToDo);
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "✅";
+  completeButton.addEventListener("click", completeToDo);
+  const buttons = document.createElement("div");
+  buttons.appendChild(deleteButton);
+  buttons.appendChild(completeButton);
   li.appendChild(span);
-  li.appendChild(button);
+  li.appendChild(buttons);
   toDoList.appendChild(li);
 }
 
