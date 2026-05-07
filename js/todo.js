dd892c7fc02051ceb5883eb16e4e5a83b9e1201f
jjmin208 <151809776+jjmin208@users.noreply.github.com>
2026-05-07 10:56:57 +0900

    260507 console

--- js/todo.js
+++ js/todo.js
@@ -28,6 +28,9 @@ function completeToDo(event) {
   completeButton.remove();
   toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
   saveToDos();
+  console.log(
+    "There is a bug where completed `<li>` elements disappear when the page is reloaded.However, the `<li>` elements in the todoList don't disappear even after reloading the page.If Nico is reading this, I would appreciate it if you could leave a suggestion for revision or any comments.",
+  );
 }
 
 function paintToDo(newTodo) {
